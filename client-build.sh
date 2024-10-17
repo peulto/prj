101697ce3186c0cd4517902053aca6569c983f71
user10312 <2024gs10312@gosaek.hs.kr>
2024-10-17 17:43:15 +0900

    chore: test

--- client-build.sh
+++ client-build.sh
@@ -5,7 +5,7 @@ npm run build
 
 cd ..
 
-cp -r client/dist server/dist/client
-cp vercel.json server/dist
+cp -r client/dist server/dist
+cp vercel.json server/dist/vercel.json
 
 echo "Success move from client/dist to server/dist/client"
