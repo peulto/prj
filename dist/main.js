1a99074bc23d98196c89d245ba1c41b4fe98634c
user10312 <2024gs10312@gosaek.hs.kr>
2024-10-18 22:07:38 +0900

    chore: test2222

--- dist/main.js
+++ dist/main.js
@@ -6,15 +6,16 @@ const path_1 = require("path");
 const app = (0, express_1.default)();
 const port = 3001;
 app.use(express_1.default.json());
-//const clientPath = (0, path_1.join)(__dirname, "./client/");
-//app.use(express_1.default.static(clientPath));
+const clientPath = (0, path_1.join)(__dirname, "./client/");
+app.use(express_1.default.static(clientPath));
 app.get('/api/hello', (req, res) => {
     res.json({ 'hello': 'world!' });
 });
 app.get('*', (req, res) => {
-    //res.sendFile((0, path_1.resolve)(clientPath, 'index.html'));
+    res.sendFile((0, path_1.resolve)(clientPath, 'index.html'));
 });
 app.listen(port, () => {
     console.log(`Server listening is http://localhost:${port}`);
 });
+module.exports = app;
 //# sourceMappingURL=main.js.map
